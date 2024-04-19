1b152cc1cdc5ef2ceacd550a9498ca15ce8cd577
its-najib <103586497+its-najib@users.noreply.github.com>
2024-04-19 15:07:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -17,15 +17,15 @@ global.autoreadgc = true;
 
 
 //
-global.prefa = ["."];                                    //Default prefix here. you can change if you want.
+global.prefa = ["!"];                                    //Default prefix here. you can change if you want.
 
 
 //
 global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
+global.OwnerName = "Najib";
+global.BotName = "Katara";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
